ca0367f031c0ee12e3d79f4e16be427f05af86a0
Fredika Putra <fredikaputra6@gmail.com>
2020-02-25 18:14:50 +0800

    Design home

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASEURL', 'http://localhost/website/lapor.com');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'db_lapor');
--- app/views/template/index.php
+++ app/views/template/index.php
@@ -3,8 +3,77 @@
 	<head>
 		<meta charset="utf-8">
 		<title></title>
+		<link rel="stylesheet" href="<?= BASEURL; ?>/assets/css/style.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 	<body>
-		this is my website
+		<nav>
+			<div class="width">
+				<div class="left">
+					<a href="<?= BASEURL; ?>">LAPOR!</a>
+				</div>
+				<div class="right">
+					<a href="" class="menu">Tentang</a>
+					<a href="" class="menu">Hubungi Kami</a>
+					<a href="" class="signin">Masuk</a>
+					<a href="" class="signup">Daftar</a>
+				</div>
+			</div>
+		</nav>
+		
+		<header>
+			<h1>Layanan Pengaduan Masyarakat Online</h1>
+			<div>
+				<a href="#">LAPOR!</a>
+			</div>
+		</header>
+		
+		<section id="lapor">
+			<h2 class="section-title">Kami Peduli</h2>
+			<span class="sub-section-title">Sampaikan laporan Anda langsung kepada instansi pemerintah berwenang</span>
+			<p>LAPOR! dibentuk untuk merealisasikan kebijakan “no wrong door policy” yang menjamin hak masyarakat agar pengaduan dari manapun dan jenis apapun akan disalurkan kepada penyelenggara pelayanan publik yang berwenang menanganinya. Hal ini bertujuan untuk dapat mengelola pengaduan dari masyarakat secara sederhana, cepat, tepat, tuntas, dan terkoordinasi dengan baik dan dapat meningkatkan kualitas pelayanan publik.</p>
+		</section>
+		
+		<section id="jumlah-laporan">
+			<div class="width">
+				<div>
+					<h2>Laporan Tercatat</h2>
+					<span>530.000</span>
+				</div>
+				<div>
+					<h2>Pelapor</h2>
+					<span>9.789</span>
+				</div>
+				<div>
+					<h2>Instansi Terhubung</h2>
+					<span>533</span>
+				</div>
+			</div>
+		</section>
+		
+		<footer>
+			<div class="width">
+				<div>
+					<a href="<?= BASEURL; ?>">LAPOR!</a>
+					<p>Kami membantu anda untuk menyalurkan aspirasi anda kepada instansi yang berwenang demi tercapainya kepuasan anda.</p>
+					<div>
+						<a href=""><i class="fa fa-facebook"></i></a>
+						<a href=""><i class="fa fa-instagram"></i></a>
+						<a href=""><i class="fa fa-youtube"></i></a>
+					</div>
+				</div>
+				<div>
+					<span>Navigasi</span>
+					<a href="">Lapor</a>
+					<a href="">Tentang</a>
+					<a href="">Hubungi Kami</a>
+				</div>
+				<div>
+					<span>Kontak</span>
+					<div><i class="fa fa-envelope fa-fw"></i><a href="">info@lapor.com</a></div>
+					<div><i class="fa fa-map-marker fa-fw"></i><a href="">Denpasar, Bali</a></div>
+				</div>
+			</div>
+		</footer>
 	</body>
 </html>
