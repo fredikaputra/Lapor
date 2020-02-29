5118889b2682d9b338e94fffc51935f4ffaf48cb
Fredika Putra <fredikaputra6@gmail.com>
2020-02-29 11:53:14 +0800

    Separate js file

--- app/views/lapor/index.php
+++ app/views/lapor/index.php
@@ -31,15 +31,4 @@
 	</form>
 </main>
 
-<script type="text/javascript">
-	var tx = document.getElementsByTagName('textarea');
-	for (var i = 0; i < tx.length; i++) {
-		tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
-		tx[i].addEventListener("input", OnInput, false);
-	}
-
-	function OnInput() {
-		this.style.height = 'auto';
-		this.style.height = (this.scrollHeight) + 'px';
-	}
-</script> 
+<script src="<?= BASEURL ?>/assets/js/autoresize.js" charset="utf-8"></script>
