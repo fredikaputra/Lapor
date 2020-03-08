df9a42907dc3b0b0dd163bf345e639df3e2a75cf
Fredika Putra <fredikaputra6@gmail.com>
2020-03-08 08:28:33 +0800

    Login modal

--- app/views/template/footer.php
+++ app/views/template/footer.php
@@ -9,6 +9,24 @@
 		</div>
 	</footer>
 	
+	<div id="modal">
+		<form class="login" method="post" action="<?= BASEURL ?>">
+			<a href="javascript:void(0)"><img src="<?= BASEURL ?>/assets/img/icon/times.png" alt=""></a>
+			<h2>Login</h2>
+			<span>Masukkan username dan password anda untuk melanjutkan</span>
+			<div>
+				<img src="<?= BASEURL ?>/assets/img/icon/user.png" alt="">
+				<input type="text" placeholder="Username" autofocus>
+			</div>
+			<div>
+				<img src="<?= BASEURL ?>/assets/img/icon/lock.png" alt="">
+				<input type="password" placeholder="Password">
+			</div>
+			<button type="submit" class="bg-flat bg-primary" name="login">Login</button>
+			<a href="<?= BASEURL ?>">Belum Punya Akun?</a>
+		</form>
+	</div>
+	
 	<?php
 	
 	if (isset($data['js'])) {
