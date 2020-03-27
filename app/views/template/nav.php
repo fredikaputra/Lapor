83d41ea1c68baaee6b0d1c4aae1ce9dc42fbb559
Fredika Putra <fredikaputra6@gmail.com>
2020-03-27 15:06:17 +0800

    account in nav

--- app/views/template/nav.php
+++ app/views/template/nav.php
@@ -6,10 +6,52 @@
 		<div class="menu">
 			<a href="<?= BASEURL ?>/beranda" <?= ($data['controller'] === 'Beranda') ? 'class="active"' : '' ?>>BERANDA</a>
 			<a href="<?= BASEURL ?>/formulir-pengaduan" <?= ($data['controller'] === 'Formulir_pengaduan') ? 'class="active"' : '' ?>>FORMULIR PENGADUAN</a>
-			<div class="unlogged">
-				<a href="<?= BASEURL ?>/login">LOGIN</a>
-				<a href="<?= BASEURL ?>/daftar">DAFTAR</a>
-			</div>
+			<?php
+			
+			if (isset($_SESSION['masyarakatNIK'])) {
+				?>
+				
+				<div class="logged">
+					<button type="button" onclick="toggleAccPan()">
+						<img src="https://image.flaticon.com/icons/svg/527/527489.svg">
+						Lada Sattar
+					</button>
+					
+					<div class="user-menu hide">
+						<a href="<?= BASEURL ?>/dashboard/pengaturan" class="sett">
+							<img src="<?= BASEURL ?>/assets/img/icon/settings.png">
+						</a>
+						<img src="https://image.flaticon.com/icons/svg/527/527489.svg">
+						<a href="<?= BASEURL ?>/dashboard/pengaturan/foto-profil" class="change">
+							<img src="<?= BASEURL ?>/assets/img/icon/camera.png">
+						</a>
+						<span class="name">I Putu Fredika Putra</span>
+						<span class="username">@redtra</span>
+						<a href="<?= BASEURL ?>/dashboard/riwayat-aduan">Riwayat Aduan</a>
+						<hr>
+						<a href="<?= BASEURL ?>/logout">Logout</a>
+					</div>
+				</div>
+				
+				<?php
+			}else {
+				?>
+				
+				<div class="unlogged">
+					<a href="<?= BASEURL ?>/login">LOGIN</a>
+					<a href="<?= BASEURL ?>/daftar">DAFTAR</a>
+				</div>
+				
+				<?php
+			}
+			
+			?>
 		</div>
 	</div>
 </nav>
+
+<script type="text/javascript">
+	function toggleAccPan(){
+		document.querySelector('.user-menu').classList.toggle('hide');
+	}
+</script>
--- index.php
+++ index.php
@@ -7,3 +7,9 @@ date_default_timezone_set('Asia/Makassar'); // atur waktu ke wita
 require_once 'app/init.php'; // panggil file init.php
 
 $app = new App; // jalankan aplikasinya
+
+var_dump($_POST);
+echo '<br />';
+var_dump($_SESSION);
+echo '<br />';
+var_dump($_FILES);
