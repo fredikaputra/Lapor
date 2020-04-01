2f6113b661dc7a1ac07d06cc6ed5ae19cd270a27
Fredika Putra <fredikaputra6@gmail.com>
2020-04-01 14:40:59 +0800

    Update profile with ux

--- app/views/dashboard/pet_dashboard.php
+++ app/views/dashboard/pet_dashboard.php
@@ -19,22 +19,22 @@
 						
 						?>
 						<label><img src="<?= BASEURL ?>/assets/img/icon/camera.png" alt="">
-							<input type="file" name="photo">
+							<input type="file" id="onChange3" onchange="checkValueChange()" name="photo" accept=".jpg">
 						</label>
 					</div>
 					<div>
 						<img src="<?= BASEURL ?>/assets/img/icon/circle-user.png" alt="">
-						<input type="text" value="<?= $data['name'] ?>" name="name">
+						<input type="text" id="onChange1" onkeyup="checkValueChange()" value="<?= $data['name'] ?>" name="name" autocomplete="off">
 					</div>
 					<div>
 						<img src="<?= BASEURL ?>/assets/img/icon/circle-phone-book.png" alt="">
-						<input type="number" value="<?= $data['phone'] ?>" name="phone">
+						<input type="number" id="onChange2" onkeyup="checkValueChange()" value="<?= $data['phone'] ?>" name="phone" autocomplete="off">
 					</div>
 					<div>
 						<a href="<?= BASEURL ?>/dashboard/change-pass">Ganti Password</a>
 						<div>
-							<button type="reset">Batal</button>
-							<button type="submit" name="updateprofile">Simpan</button>
+							<button type="reset" class="hide" onclick="hide()" id="cancelSaveUserProfile">Batal</button>
+							<button type="submit" class="hide" name="updateprofile" id="saveUserProfile">Simpan</button>
 						</div>
 					</div>
 				</form>
@@ -70,3 +70,32 @@
 		</div>
 	</div>
 </main>
+
+<script type="text/javascript">
+	var saveBtn = document.getElementById('saveUserProfile');
+	var cancelSaveBtn = document.getElementById('cancelSaveUserProfile');
+
+	function checkValueChange(){
+		var defaultVal1 = document.getElementById('onChange1').defaultValue;
+		var curentVal1 = document.getElementById('onChange1').value;
+		var defaultVal2 = document.getElementById('onChange2').defaultValue;
+		var curentVal2 = document.getElementById('onChange2').value;
+		var defaultVal3 = document.getElementById('onChange3').defaultValue;
+		var curentVal3 = document.getElementById('onChange3').value;
+		
+		// check if value is change
+		if (defaultVal1 == curentVal1 && defaultVal2 == curentVal2 && defaultVal3 == curentVal3) {
+			saveBtn.classList.add('hide');
+			cancelSaveBtn.classList.add('hide');
+		}else {
+			saveBtn.classList.remove('hide');
+			cancelSaveBtn.classList.remove('hide');
+		}
+	}
+
+	// hide function
+	function hide(){
+		saveBtn.classList.add('hide');
+		cancelSaveBtn.classList.add('hide');
+	}
+</script>
