69566f8dd813f88cf2b980c63b7619c815e0d72d
Fredika Putra <fredikaputra6@gmail.com>
2020-04-02 15:59:36 +0800

    give back button

--- app/views/pengaturan/nama.php
+++ app/views/pengaturan/nama.php
@@ -5,6 +5,9 @@
 	<h2>Informasi Pribadi</h2>
 	<span>Informasi dasar anda seperti nama dan foto.</span>
 	<div>
+		<a href="<?= BASEURL ?>/pengaturan">
+			<img src="<?= BASEURL ?>/assets/img/icon/left-arrow.png" alt="">
+		</a>
 		<h3>Nama</h3>
 		
 		<form action="<?= BASEURL ?>/pengaturan/proccess" method="post">
