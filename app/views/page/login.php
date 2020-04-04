6d7452099280bcad5fad60d61b43bd91ff8befb2
Fredika Putra <fredikaputra6@gmail.com>
2020-04-04 22:16:28 +0800

    add required

--- app/views/page/login.php
+++ app/views/page/login.php
@@ -6,10 +6,12 @@
 		<h1>Login</h1>
 		<span>Hallo, silahkan untuk masukkan username dan password anda!</span>
 		<div title="Masukkan username anda">
-			<img src="<?= BASEURL ?>/assets/img/icon/circle-user.png"><input type="text" placeholder="Username" name="username" autofocus>
+			<img src="<?= BASEURL ?>/assets/img/icon/circle-user.png">
+			<input type="text" placeholder="Username" name="username" required autofocus>
 		</div>
 		<div title="Masukkan password anda">
-			<img src="<?= BASEURL ?>/assets/img/icon/circle-padlock.png"><input type="password" placeholder="Password" name="password">
+			<img src="<?= BASEURL ?>/assets/img/icon/circle-padlock.png">
+			<input type="password" placeholder="Password" name="password" required>
 		</div>
 		<button type="submit" name="login">LOGIN</button>
 		<span>Belum punya akun? <a href="<?= BASEURL ?>/daftar">Daftar</a> sekarang!</span>
