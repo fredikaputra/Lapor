a7df379227a71742845100cd675c057c7618f576
Fredika Putra <fredikaputra6@gmail.com>
2020-04-04 17:20:15 +0800

    Undone

--- app/views/dashboard/data_aduan.php
+++ app/views/dashboard/data_aduan.php
@@ -1,64 +1,47 @@
 	<div class="content">
 		<div>
 			<h2>Data Aduan</h2>
-			<table>
-				<thead>
-					<tr>
-						<td>Waktu</td>
-						<td>Dari</td>
-						<td>Isi Laporan</td>
-						<td>Foto</td>
-						<td>Status</td>
-						<td></td>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					
-					if ($data['laporan'] == !NULL) {
-						foreach ($data['laporan'] as $laporan) {
-							?>
-							
-							<tr>
-								<td><?= strftime("%A, %d %B %Y", $data['laporan'][0]['tgl_pengaduan']) ?></td>
-								<td><?= $laporan['nama'] ?></td>
-								<td><p><?= $laporan['isi_laporan'] ?></p></td>
-								<td>
-									<?php
-									
-									if ($laporan['foto'] == NULL) {
-										echo 'Tidak ada gambar';
-									}else {
-										?>
-										
-										<button type="button">Lihat Gambar</button>
-										
-										<?php
-									}
-									
-									?>
-								</td>
-								<td>
-									<?php
-									
-									if ($laporan['status'] == '0') {
-										echo 'Dalam Proses';
-									}else if ($laporan['status'] == '1') {
-										echo 'Selesai';
-									}
-									
-									?>
-								</td>
-								<td><a href="<?= BASEURL ?>/dashboard/data-aduan/<?= $laporan['id_pengaduan'] ?>">Tinjau</a></td>
-							</tr>
-							
-							<?php
-						}
+			<span>Semua data aduan yang masuk dari masyarakat</span>
+			<div class="data">
+				<?php
+				
+				if ($data['laporan'] == !NULL) {
+					foreach ($data['laporan'] as $laporan) {
+						?>
+						
+						<div>
+							<div>
+								<input type="checkbox">
+							</div>
+							<div>
+								<span><?= $laporan['nama'] ?></span>
+							</div>
+							<div>
+								<p><?= $laporan['isi_laporan'] ?></p>
+							</div>
+							<div>
+								<?= strftime("%A, %d %B %Y", $data['laporan'][0]['tgl_pengaduan']) ?>
+							</div>
+							<div class="action">
+								<a href="<?= BASEURL ?>/dashboard/data-aduan/<?= $laporan['id_pengaduan'] ?>">
+									<img src="<?= BASEURL ?>/assets/img/icon/eye.png">
+								</a>
+								<a href="<?= BASEURL ?>/dashboard/data-aduan/<?= $laporan['id_pengaduan'] ?>">
+									<img src="<?= BASEURL ?>/assets/img/icon/print.png">
+								</a>
+								<a href="<?= BASEURL ?>/dashboard/data-aduan/<?= $laporan['id_pengaduan'] ?>">
+									<img src="<?= BASEURL ?>/assets/img/icon/bin.png">
+								</a>
+							</div>
+						</div>
+						
+						<?php
 					}
-					
-					?>
-				</tbody>
-			</table>
+				}
+				
+				?>
+			</div>
 		</div>
 	</div>
 </main>
+
--- app/views/riwayat_aduan/index.php
+++ app/views/riwayat_aduan/index.php
@@ -4,40 +4,40 @@
 <main>
 	<h2>Riwayat Aduan</h2>
 	<span>Semua data aduan anda</span>
-	<table>
-		<thead>
-			<tr>
-				<td>#</td>
-				<td>Waktu</td>
-				<td>Isi Laporan</td>
-				<td>Foto</td>
-				<td>Status</td>
-				<td></td>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			
-			if (isset($data['laporan'])) {
-				$no = 1;
-				foreach ($data['laporan'] as $laporan) {
-					?>
-					
-					<tr>
-						<td><?= $no ?></td>
-						<td><?= date('l, d F Y', $laporan['tgl_pengaduan']) ?></td>
-						<td><p><?= $laporan['isi_laporan'] ?></p></td>
-						<td><?= (isset($laporan['foto'])) ? '1 Gambar' : 'Tidak ada gambar' ?></td>
-						<td><?= ($laporan['status'] == 0) ? 'Dalam Proses' : 'Selesai' ?></td>
-						<td><a href="<?= BASEURL ?>/riwayat-aduan/detail/<?= $laporan['id_pengaduan'] ?>">Lihat</a></td>
-					</tr>
-					
-					<?php
-					$no++;
+	<div>
+		<table>
+			<thead>
+				<tr>
+					<td>#</td>
+					<td>Waktu</td>
+					<td>Isi Laporan</td>
+					<td>Status</td>
+					<td></td>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				
+				if (isset($data['laporan'])) {
+					$no = 1;
+					foreach ($data['laporan'] as $laporan) {
+						?>
+						
+						<tr>
+							<td><?= $no ?></td>
+							<td><?= date('l, d F Y', $laporan['tgl_pengaduan']) ?></td>
+							<td><p><?= $laporan['isi_laporan'] ?></p></td>
+							<td><?= ($laporan['status'] == 0) ? 'Dalam Proses' : 'Selesai' ?></td>
+							<td><a href="<?= BASEURL ?>/riwayat-aduan/detail/<?= $laporan['id_pengaduan'] ?>">Lihat</a></td>
+						</tr>
+						
+						<?php
+						$no++;
+					}
 				}
-			}
-			
-			?>
-		</tbody>
-	</table>
+				
+				?>
+			</tbody>
+		</table>
+	</div>
 </main>
